7cd43b31e17d5a10891c112ea3a7da0999c1c0b2
Eduardo Kraus <kraus@eduardokraus.com>
2025-04-06 22:42:05 -0300

    V 2.3.2

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025040600;
+$plugin->version = 2025040601;
 $plugin->requires = 2019052000;
-$plugin->release = "2.3.1";
+$plugin->release = "2.3.2";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "local_helpdesk";
 
