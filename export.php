30916ac8d97cb49cc87ade1c8b57f1b04f8f85e3
Eduardo Kraus <kraus@eduardokraus.com>
2026-04-01 09:53:22 -0300

    copyright

--- export.php
+++ export.php
@@ -14,6 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * setting file
+ *
+ * @package   local_helpdesk
+ * @copyright 2026 Smartlearn-edu {@link https://github.com/Smartlearn-edu}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 require_once(__DIR__ . "/../../config.php");
 require_once(__DIR__ . "/lib.php");
 
