10d30f15e65686156345e38a56447129ecb28a24
Eduardo Kraus <kraus@eduardokraus.com>
2026-08-12 14:51:47 -0300

    Implement unanswered ticket reminder task

--- classes/mail/ticket_mail.php
+++ classes/mail/ticket_mail.php
@@ -5,17 +5,9 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * ticket file
+ * Ticket email notifications.
  *
  * @package   local_helpdesk
  * @copyright 2025 Eduardo Kraus {@link https://eduardokraus.com}
@@ -31,142 +23,116 @@ use local_helpdesk\util\files;
 use local_kopere_dashboard\output\events\event_info;
 
 /**
- * Class ticket_mail
- *
- * @package local_helpdesk\mail
+ * Ticket mail service.
  */
 class ticket_mail {
 
     /**
-     * Function send
+     * Build the common event variables used by helpdesk email templates.
      *
      * @param ticket $ticket
-     *
-     * @throws \coding_exception
-     * @throws \dml_exception
+     * @param string $text
+     * @param array $attachments
+     * @return object
      */
-    public function send_ticket(ticket $ticket) {
+    private function build_event(ticket $ticket, $text, array $attachments) {
         global $OUTPUT, $CFG;
 
-        $sendevents = new send_message();
-
-        $files = files::all("ticket", $ticket->get_id());
-        $event = (object)[
+        return (object)[
             "categoryname" => $ticket->get_category()->get_name(),
-            "categorylink" => $OUTPUT->render_from_template("local_helpdesk/mail-link",
-                [
-                    "link" => "{$CFG->wwwroot}/local/helpdesk/?find_category={$ticket->get_categoryid()}",
-                    "text" => $ticket->get_category()->get_name(),
-                ]),
+            "categorylink" => $OUTPUT->render_from_template("local_helpdesk/mail-link", [
+                "link" => "{$CFG->wwwroot}/local/helpdesk/?find_category={$ticket->get_categoryid()}",
+                "text" => $ticket->get_category()->get_name(),
+            ]),
             "subjectname" => $ticket->get_subject(),
-            "subjectlink" => $OUTPUT->render_from_template("local_helpdesk/mail-link",
-                [
-                    "link" => "{$CFG->wwwroot}/local/helpdesk/ticket.php?id={$ticket->get_idkey()}",
-                    "text" => $ticket->get_subject(),
-                ]),
-            "helpdesk" => $OUTPUT->render_from_template("local_helpdesk/mail-link",
-                [
-                    "link" => "{$CFG->wwwroot}/local/helpdesk/",
-                    "text" => get_string("pluginname", "local_helpdesk"),
-                ]),
+            "subjectlink" => $OUTPUT->render_from_template("local_helpdesk/mail-link", [
+                "link" => "{$CFG->wwwroot}/local/helpdesk/ticket.php?id={$ticket->get_idkey()}",
+                "text" => $ticket->get_subject(),
+            ]),
+            "helpdesk" => $OUTPUT->render_from_template("local_helpdesk/mail-link", [
+                "link" => "{$CFG->wwwroot}/local/helpdesk/",
+                "text" => get_string("pluginname", "local_helpdesk"),
+            ]),
             "tiketidname" => $ticket->get_idkey(),
-            "tiketidlink" => $OUTPUT->render_from_template("local_helpdesk/mail-link",
-                [
-                    "link" => "{$CFG->wwwroot}/local/helpdesk/ticket.php?id={$ticket->get_idkey()}",
-                    "text" => $ticket->get_idkey(),
-                ]),
-            "text" => $ticket->get_description(),
-            "attachment" => $OUTPUT->render_from_template("local_helpdesk/mail-attachment",
-                [
-                    "responsefiles_count" => count($files),
-                    "responsefiles" => $files,
-                ]),
+            "tiketidlink" => $OUTPUT->render_from_template("local_helpdesk/mail-link", [
+                "link" => "{$CFG->wwwroot}/local/helpdesk/ticket.php?id={$ticket->get_idkey()}",
+                "text" => $ticket->get_idkey(),
+            ]),
+            "text" => $text,
+            "attachment" => $OUTPUT->render_from_template("local_helpdesk/mail-attachment", [
+                "responsefiles_count" => count($attachments),
+                "responsefiles" => $attachments,
+            ]),
         ];
-        $sendevents->set_event($event);
+    }
+
+    /**
+     * Send new ticket notification.
+     *
+     * @param ticket $ticket
+     */
+    public function send_ticket(ticket $ticket) {
+        $sendevents = new send_message();
+        $files = files::all("ticket", $ticket->get_id());
+        $sendevents->set_event($this->build_event($ticket, $ticket->get_description(), $files));
 
         $kopereevent = new event_info();
         $kopereevent->userfrom = "admin";
         $kopereevent->subject = get_string("mailticket_subject", "local_helpdesk");
 
-        // To Category Users.
         $kopereevent->message = get_string("mailticket_create_message", "local_helpdesk");
         $sendevents->set_event_info($kopereevent);
-
         $categoryusers = category_users::get_all(null, ["categoryid" => $ticket->get_categoryid()]);
         $sendevents->send_mail($ticket, $categoryusers);
 
-        // To Creator User.
         $kopereevent->message = get_string("mailticket_user_message", "local_helpdesk");
         $sendevents->set_event_info($kopereevent);
-
-        $categoryusers = [new category_users(["userid" => $ticket->get_userid()])];
-        $sendevents->send_mail($ticket, $categoryusers);
+        $sendevents->send_mail($ticket, [new category_users(["userid" => $ticket->get_userid()])]);
     }
 
     /**
-     * Function send_response
+     * Send response notification.
      *
      * @param ticket $ticket
      * @param response $response
-     *
-     * @throws \coding_exception
-     * @throws \dml_exception
      */
     public function send_response(ticket $ticket, response $response) {
-        global $OUTPUT, $CFG;
-
         $sendevents = new send_message();
-
         $files = files::all("response", $response->get_id());
-        $event = (object)[
-            "categoryname" => $ticket->get_category()->get_name(),
-            "categorylink" => $OUTPUT->render_from_template("local_helpdesk/mail-link",
-                [
-                    "link" => "{$CFG->wwwroot}/local/helpdesk/?find_category={$ticket->get_categoryid()}",
-                    "text" => $ticket->get_category()->get_name(),
-                ]),
-            "subjectname" => $ticket->get_subject(),
-            "subjectlink" => $OUTPUT->render_from_template("local_helpdesk/mail-link",
-                [
-                    "link" => "{$CFG->wwwroot}/local/helpdesk/ticket.php?id={$ticket->get_idkey()}",
-                    "text" => $ticket->get_subject(),
-                ]),
-            "helpdesk" => $OUTPUT->render_from_template("local_helpdesk/mail-link",
-                [
-                    "link" => "{$CFG->wwwroot}/local/helpdesk/",
-                    "text" => get_string("pluginname", "local_helpdesk"),
-                ]),
-            "tiketidname" => $ticket->get_idkey(),
-            "tiketidlink" => $OUTPUT->render_from_template("local_helpdesk/mail-link",
-                [
-                    "link" => "{$CFG->wwwroot}/local/helpdesk/ticket.php?id={$ticket->get_idkey()}",
-                    "text" => $ticket->get_idkey(),
-                ]),
-            "text" => $response->get_message(),
-            "attachment" => $OUTPUT->render_from_template("local_helpdesk/mail-attachment",
-                [
-                    "responsefiles_count" => count($files),
-                    "responsefiles" => $files,
-                ]),
-        ];
-        $sendevents->set_event($event);
+        $sendevents->set_event($this->build_event($ticket, $response->get_message(), $files));
 
         $kopereevent = new event_info();
         $kopereevent->userfrom = "admin";
         $kopereevent->subject = "Re: " . get_string("mailticket_subject", "local_helpdesk");
 
-        // To Category Users.
         $kopereevent->message = get_string("mailticket_update_message", "local_helpdesk");
         $sendevents->set_event_info($kopereevent);
-
         $categoryusers = category_users::get_all(null, ["categoryid" => $ticket->get_categoryid()]);
         $sendevents->send_mail($ticket, $categoryusers);
 
-        // To Creator User.
         $kopereevent->message = get_string("mailticket_user_message", "local_helpdesk");
         $sendevents->set_event_info($kopereevent);
+        $sendevents->send_mail($ticket, [new category_users(["userid" => $ticket->get_userid()])]);
+    }
 
-        $categoryusers = [new category_users(["userid" => $ticket->get_userid()])];
+    /**
+     * Remind the support users assigned to the ticket category when no support answer
+     * has been registered after three days. The ticket owner is not emailed by this task.
+     *
+     * @param ticket $ticket
+     */
+    public function send_unanswered_reminder(ticket $ticket) {
+        $sendevents = new send_message();
+        $files = files::all("ticket", $ticket->get_id());
+        $sendevents->set_event($this->build_event($ticket, $ticket->get_description(), $files));
+
+        $kopereevent = new event_info();
+        $kopereevent->userfrom = "admin";
+        $kopereevent->subject = get_string("mailticket_reminder_subject", "local_helpdesk");
+        $kopereevent->message = get_string("mailticket_reminder_message", "local_helpdesk");
+        $sendevents->set_event_info($kopereevent);
+
+        $categoryusers = category_users::get_all(null, ["categoryid" => $ticket->get_categoryid()]);
         $sendevents->send_mail($ticket, $categoryusers);
     }
 }
--- classes/task/response_tickets.php
+++ classes/task/response_tickets.php
@@ -5,17 +5,9 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Privacy Subsystem implementation for local_helpdesk.
+ * Remind support about tickets unanswered for more than three days.
  *
  * @package   local_helpdesk
  * @copyright 2025 Eduardo Kraus {@link https://eduardokraus.com}
@@ -24,26 +16,52 @@
 
 namespace local_helpdesk\task;
 
+use local_helpdesk\mail\ticket_mail;
+use local_helpdesk\model\ticket;
+
 /**
- * Class response_tickets
- *
- * @package local_helpdesk\task
+ * Sends a single reminder to the category support users for an unanswered ticket.
  */
 class response_tickets extends \core\task\scheduled_task {
 
-    /**
-     * Get a descriptive name for the task (shown to admins)
-     *
-     * @return string
-     */
     public function get_name() {
         return "Send email when a Ticket has been unanswered for more than 3 days";
     }
 
-    /**
-     * Do the job.
-     * Throw exceptions on errors (the job will be retried).
-     */
     public function execute() {
+        global $DB;
+
+        $cutoff = time() - (3 * DAYSECS);
+        $sql = "
+            SELECT id
+              FROM {local_helpdesk_ticket}
+             WHERE status IN (:openstatus, :progressstatus)
+               AND createdat <= :cutoff
+               AND (answeredat IS NULL OR answeredat = 0)
+               AND (remindedat IS NULL OR remindedat = 0)
+          ORDER BY createdat ASC";
+
+        $records = $DB->get_records_sql($sql, [
+            "openstatus" => ticket::STATUS_OPEN,
+            "progressstatus" => ticket::STATUS_PROGRESS,
+            "cutoff" => $cutoff,
+        ]);
+
+        $mail = new ticket_mail();
+        foreach ($records as $record) {
+            $ticket = ticket::get_by_id($record->id);
+            if (!$ticket || $ticket->get_answeredat() || $ticket->get_remindedat()) {
+                continue;
+            }
+
+            try {
+                $mail->send_unanswered_reminder($ticket);
+                $ticket->set_remindedat(time());
+                $ticket->set_updatedat(time());
+                $ticket->save();
+            } catch (\Throwable $e) {
+                mtrace("Helpdesk reminder failed for ticket {$record->id}: {$e->getMessage()}");
+            }
+        }
     }
 }
